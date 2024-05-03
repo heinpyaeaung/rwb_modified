c9a522d4be8c1c7dc9cf40d99bbd3388ad2ede86
Eddins <71978117+heinpyaeaung@users.noreply.github.com>
2024-05-03 22:56:02 +0630

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 module.exports = function(){
-    mongoose.connect(process.env.LOCALDB)
+    mongoose.connect(process.env.DATABASE)
         .then(() => console.log('Connected to MongoDB'))
         .catch(err => console.log(err.message))
 }
